396c037d81aa5d0d35017812e5cd687e6b986f67
森田賢二 <kenjimoritata@gmail.com>
2018-09-07 21:32:26 +0900

    init nex.js

---
+++ next-docker/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+export default () => (
+  <div>
+    <h1>About page</h1>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </div>
+);
