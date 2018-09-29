880f9f0f753ced4a82cadd0b759894287b8e1d75
森田賢二 <kenjimoritata@gmail.com>
2018-09-29 09:45:48 +0900

    add

--- CRNAExpoTSExample/src/App.tsx
+++ CRNAExpoTSExample/src/App.tsx
@@ -11,7 +11,7 @@ export default class App extends React.Component {
           <Header title="Awesomest App Ever!!!"  />
         </View>
         <View style={content}>
-          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Opfafaen up App.js to start working on your app!</Text>
           <Text>Changes you make will automatically reload.</Text>
           <Text>Shake your phone to open the developer menu.</Text>
         </View>
