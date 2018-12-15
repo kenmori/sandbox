975c29ceff06849fb83e7052b656de52a9be7e45
moriken <>
2018-12-15 20:19:18 +0900

    add

--- ts-app/src/App.js
+++ ts-app/src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Switch } from "react-router";
+import { Provider } from "react-redux";
+import { ConnectedRouter as Router } from "connected-react-router";
+import store from "./configStore";
+import Routes from "./routes";
+import Root from "./container/Root";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = props => (
+  <Provider store={store}>
+    <Router history={props.history}>
+      {/* <ErrorBoundary> */}
+      <Switch>
+        <Route path="/fafa" render={() => <Root {...props} />} />
+      </Switch>
+      {/* </ErrorBoundary> */}
+    </Router>
+  </Provider>
+);
 
 export default App;
