f47b06b0b30b77eda199726f8ce2b932ade5a7ed
kenjimorita <>
2019-03-10 16:09:25 +0900

    add apollo

--- graphqlwithreact/udemy-morita-graphql/src/App.js
+++ graphqlwithreact/udemy-morita-graphql/src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import client from "./client";
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-console.log({ GITHUB_TOKEN });
+const ql = gql`
+  query me {
+    user(login: "kenmori") {
+      id
+      user
+      bol
+    }
+  }
+`;
 class App extends Component {
   render() {
-    return <div className="App">Hello GraphQL</div>;
+    return;
+    <ApolloProvider client={client}>
+      <div className="App">Hello GraphQL</div>
+      <Query query={ql}>
+        {(loading, error, data) => {
+          if (loading) return "loading";
+          if (error) return error.messeage;
+          return data.name;
+        }}
+      </Query>
+    </ApolloProvider>;
   }
 }
 
