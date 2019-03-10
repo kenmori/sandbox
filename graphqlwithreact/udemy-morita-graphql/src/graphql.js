74077760f2b851d85a54af110bb31688017b463f
kenjimorita <>
2019-03-10 20:19:33 +0900

    apply query and valiables

--- graphqlwithreact/udemy-morita-graphql/src/graphql.js
+++ graphqlwithreact/udemy-morita-graphql/src/graphql.js
@@ -8,3 +8,44 @@ export const ME = gql`
     }
   }
 `;
+
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories(
+    $first: Int
+    $after: String
+    $before: String
+    $last: Int
+    $query: String!
+  ) {
+    search(
+      first: $first
+      after: $after
+      before: $before
+      last: $last
+      query: $query
+      type: REPOSITORY
+    ) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`;
