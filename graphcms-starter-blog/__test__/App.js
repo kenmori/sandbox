3a2453f4bd60ffa99c739b0f72b633e93fe4bba4
kenjimorita <>
2019-04-30 15:46:44 +0900

    update

--- graphcms-starter-blog/__test__/App.js
+++ graphcms-starter-blog/__test__/App.js
@@ -1,34 +1,34 @@
-// import { MockedProvider } from 'react-apollo/test-utils';
-// import { MemoryRouter } from 'react-router-dom';
-// import React, {createContext} from 'react';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { MemoryRouter } from 'react-router-dom';
+import React, {createContext} from 'react';
 
-// const AppContext = createContext();
+const AppContext = createContext();
 
-// export const withAppContext = (Component) => {
-//   return (props) => (
-//     <AppContext.Consumer>
-//       {context => <Component {...props} context={context}/>}
-//     </AppContext.Consumer>
-//   );
-// };
+export const withAppContext = (Component) => {
+  return (props) => (
+    <AppContext.Consumer>
+      {context => <Component {...props} context={context}/>}
+    </AppContext.Consumer>
+  );
+};
 
-// export const TestApp = (props) => {
-//     const {
-//       mocks = [],
-//       store = {},
-//       children
-//     } = props;
-//     return (
-//       <MockedProvider mocks={mocks} addTypename={false}>
-//         <MemoryRouter>
-//           <AppContext.Provider value={{
-//             store,
-//             dispatch: (action) => { return action },
-//             routeRef: React.createRef()
-//           }}>
-//             {children()}
-//           </AppContext.Provider>
-//         </MemoryRouter>
-//       </MockedProvider>
-//     );
-//   };
+export const TestApp = (props) => {
+    const {
+      mocks = [],
+      store = {},
+      children
+    } = props;
+    return (
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <MemoryRouter>
+          <AppContext.Provider value={{
+            store,
+            dispatch: (action) => { return action },
+            routeRef: React.createRef()
+          }}>
+            {children}
+          </AppContext.Provider>
+        </MemoryRouter>
+      </MockedProvider>
+    );
+  };
