1cf9b7cbab10cd65284b3698aff8aa04b3860301
kenjimorita <>
2019-05-02 21:03:00 +0900

    add

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import LinkList from './LinkList'
+import CreateLink from './CreateLink'
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return <CreateLink />
   }
 }
 
