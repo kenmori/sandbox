612023c2b05c8958f9d965368951f943426a7e37
kenjimorita <>
2019-05-18 20:13:29 +0900

    add

--- graphqlwithreact/udemy-morita-graphql/src/App.js
+++ graphqlwithreact/udemy-morita-graphql/src/App.js
@@ -39,6 +39,15 @@ class App extends Component {
       before: null
     });
   }
+  goPreviousPage(search){
+    this.setState({
+      first: null,
+      after: null,
+      last: PER_PAGE,
+      before: search.pageInfo.startCursor
+
+    })
+  }
   render() {
     const { query, first, last, before, after } = this.state;
     console.log({ query });
@@ -81,6 +90,9 @@ class App extends Component {
                     );
                   })}
                 </ul>
+                {search.pageInfo.hasPreviousPage ? (
+                  <button onClick={this.goPreviousPage.bind(this, search)}>Previous</button>
+                ) : null}
                 {search.pageInfo.hasNextPage ? (
                   <button onClick={this.goNext.bind(this, search)}>Next</button>
                 ) : null}
