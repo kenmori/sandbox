191ba0c2904a3f0fb7423e9d44200f85d92898aa
kenjimorita <>
2019-05-18 21:43:37 +0900

    writeQuery

--- graphqlwithreact/udemy-morita-graphql/src/App.js
+++ graphqlwithreact/udemy-morita-graphql/src/App.js
@@ -16,18 +16,38 @@ const DEFAULT_STATE = {
 
 const StarButton = (props) => {
   const node = props.node
-  const {query, first, last, before, after} = props;
+  const { query, first, last, before, after } = props;
   const totalCount = node.stargazers.totalCount;
   const viewerHasStarred = node.viewerHasStarred;
   const starCount = totalCount === 1 ? '1 star' : `${totalCount} stars`;
   const StarStatus = ({addorRemoveStar}) => (
-    <button onClick={() => addorRemoveStar({variables: {input: {starrableId: node.id}}})}>
+    <button onClick={() => addorRemoveStar({
+      variables: {input: {starrableId: node.id}},
+      update: (store)=> {
+        const data = store.readQuery({
+          query: SEARCH_REPOSITORIES,
+          variables: { query, first, last, before, after }
+        });
+        const edges = data.search.edges;
+        const newEdges = edges.map(edge => {
+           if(edge.node.id === node.id){
+             const totalCount = edge.node.stargazers.totalCount;
+             const diff = viewerHasStarred ? -1 : 1
+             const newTotalCount = totalCount + diff
+             edge.node.stargazers.totalCount = newTotalCount;
+           }
+           return edge
+        })
+        data.search.edges = newEdges;
+        store.writeQuery({query: SEARCH_REPOSITORIES, data})
+      }
+     }
+    )}>
       {starCount} | {viewerHasStarred ? "stared" : "-"}
     </button>
   )
   return  (
     <Mutation mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
-    refetchQueries={[{query: SEARCH_REPOSITORIES, variables: { query, first, last, before, after }}]}
     >
     {(addorRemoveStar) => <StarStatus addorRemoveStar={addorRemoveStar} />}
     </Mutation>
@@ -52,7 +72,6 @@ class App extends Component {
   }
 
   goNext(search) {
-    console.log(search.pageInfo, "search.pageInfo.endCoursor");
     this.setState({
       first: PER_PAGE,
       after: search.pageInfo.endCursor,
@@ -71,7 +90,6 @@ class App extends Component {
   }
   render() {
     const { query, first, last, before, after } = this.state;
-    console.log({ query });
     return (
       <ApolloProvider client={client}>
         <form onSubmit={this.handleSubmit}>
@@ -84,7 +102,6 @@ class App extends Component {
           {({ loading, error, data }) => {
             if (loading) return "loading";
             if (error) return `Error! ${error.message}`;
-            console.log({ data });
             const search = data.search;
             const repositoryCount = search.repositoryCount;
             const repositoryUnit =
