9fd92ea675c18be5090679657c629dd977fceab6
kenjimorita <kenjimoritata@gmail.com>
2020-06-27 15:13:53 +0900

    fix

---
+++ graphql/server/models/movie.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const movieSchema = new Schema({
+  name: String,
+  genre: String
+})
+
+// modelの作成
+module.exports =  mongoose.model("Movie", movieSchema) // model name / schema
