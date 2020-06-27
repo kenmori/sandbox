c1f0c70716a9bdeae6aa668eb05507793290f7bd
kenjimorita <kenjimoritata@gmail.com>
2020-06-27 15:34:03 +0900

    add

--- graphql/server/app.js
+++ graphql/server/app.js
@@ -1,16 +1,17 @@
 const express = require("express")
-const graphqlHTTP = require("express-graphql")
+const express_graphql = require("express-graphql")
 const mongoose = require("mongoose")
 const schema = require("./schema/schema.js")
 const app = express()
-mongoose.connect("")
+
+mongoose.connect("mongodb+srv://admin:ok365a1@cluster0-mwt3x.mongodb.net/test?retryWrites=true&w=majority")
 mongoose.connection.once("open", () => {
   console.log("db connect");
 })
 // endopoint
-app.use("/graphql", graphqlHTTP({// このなかにschemaを定義する
+app.use("/graphql", express_graphql({// このなかにschemaを定義する
   schema,
-  graphiql: true
+  graphiql: true // backendだけでtestができるように
 }))
 app.listen(4000, () => {
   console.log("listengin listerner");
