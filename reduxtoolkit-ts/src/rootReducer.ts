94fa421197377ee81293ae84c4b4ccf57ef776a7
kenjimorita <kenjimoritata@gmail.com>
2020-10-15 20:00:05 +0900

    feat: slice

--- reduxtoolkit-ts/src/rootReducer.ts
+++ reduxtoolkit-ts/src/rootReducer.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit"
+import * as Counter from "./features/counterSlice"
 
-const rootReducer = combineReducers({
+type counterStore = ReturnType<Counter.reducers>
+const rootReducer = combineReducers<counterStore>({
+  counter: Counter.reducers
 })
 
 export type RootState = ReturnType<typeof rootReducer>
